182f6eb8a070291496f26cb50b5173dbdf2535e5
showAwohs <m2aman.interesmation.0322@gmail.com>
2018-05-01 14:34:42 +0900

    db

--- db/migrate/3_create_drafts.rb
+++ db/migrate/3_create_drafts.rb
@@ -1,6 +1,7 @@
 class CreateDrafts < ActiveRecord::Migration[5.1]
   def change
     create_table :drafts do |t|
+      t.string :title, null: false
 
       t.timestamps
     end
--- db/migrate/5_create_notifications.rb
+++ db/migrate/5_create_notifications.rb
@@ -1,6 +1,7 @@
 class CreateNotifications < ActiveRecord::Migration[5.1]
   def change
     create_table :notifications do |t|
+      t.string :title, null: false
 
       t.timestamps
     end
