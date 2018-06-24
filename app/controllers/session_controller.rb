645a873b6d30053499088db857507d4e3eed989e
showAwohs <m2aman.interesmation.0322@gmail.com>
2018-06-24 14:11:39 +0900

    register

--- app/controllers/session_controller.rb
+++ app/controllers/session_controller.rb
@@ -15,12 +15,17 @@ class SessionController < BaseController
     end
 
     def register
-        user = User.new(params.permit(:name,:email,:password,:password_confirmation))
+        user = User.new(user_params)
         if user.save
             session[:id] = user.id
             return redirect_to '/profiles/me'
         end
         return render :signUp
+    end
+
+    private
+    def user_params
+        params.permit(:name,:email,:password,:password_confirmation)
     end
 end
   
